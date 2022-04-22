489186ea763593330fe41e0a8758393f8d61cdc7
Justin Ramos <jramos20022@gmail.com>
2022-04-21 20:08:33 -0400

    inquirer installed

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+inquirer 
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
 
-const profileDataArgs = process.argv.slice(2);
+  .then(answers => console.log(answers));
+//const fs = require('fs');
 
-const [name, github] = profileDataArgs;
-
-/* new commit */
+//const generatePage = require('./src/page-template');
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//const pageHTML = generatePage(name, github);
+
+
+//fs.writeFile('index.html', pageHTML, err => {
+ //   if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+ //   console.log('Portfolio complete! Check out index.html to see the output!');
+ // });
+
