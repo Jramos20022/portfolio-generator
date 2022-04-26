eb1263f18088e1a130bfcb97bcfe87643ffcba0d
Justin Ramos <jramos20022@gmail.com>
2022-04-26 18:35:02 -0400

    HTML functioning

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -62,7 +66,8 @@ return inquirer.prompt([
     if(!portfolioData.projects){
       portfolioData.projects = [];
     }
-    return inquirer.prompt([
+    return inquirer
+    .prompt([
       {
         type: 'input',
         name: 'name',
@@ -136,20 +141,19 @@ return inquirer.prompt([
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+  
+    const pageHTML = generatePage(portfolioData);
+
+
+fs.writeFile('index.html', pageHTML, err => {
+ if (err) throw err;
+  
+  console.log('Portfolio complete! Check out index.html to see the output!');
   });
 
-//const fs = require('fs');
 
-//const generatePage = require('./src/page-template');
-
-
-//const pageHTML = generatePage(name, github);
+  });
 
 
-//fs.writeFile('index.html', pageHTML, err => {
- //   if (err) throw err;
-  
- //   console.log('Portfolio complete! Check out index.html to see the output!');
- // });
+
 
